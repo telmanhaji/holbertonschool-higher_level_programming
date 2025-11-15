c58837c6b721a23a289f6010aedeaa3d9f4845aa
telmanhaji <ibliandr@gmail.com>
2025-11-15 21:06:34 +0400

    8-uppercase.py - function that prints a string in uppercase followed by a new line.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 8-uppercase.py - function that prints a string in uppercase followed by a new line.
+# function that prints a string in uppercase followed by a new line.
 def uppercase(str):
     for c in str:
         num = ord(c)
