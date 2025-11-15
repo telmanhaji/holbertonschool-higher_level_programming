251418cfbdf76500650c25009463b349f28e859d
telmanhaji <ibliandr@gmail.com>
2025-11-15 18:09:35 +0400

    script 2-print_alphabet.py that print all alphabet without adding new line

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# This script loops through ASCII numbers 97-122
+# and prints their character values.
+
+for i in range(97, 123):
+    # chr(i) turns the number (e.g., 97) into a letter ('a')
+    # end="" prints it without a new line, so they all
+    # appear on the same line.
+    print(chr(i), end="")
