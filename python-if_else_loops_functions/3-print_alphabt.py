db407b937d685543bb79e0ecab949955b561b598
telmanhaji <ibliandr@gmail.com>
2025-11-15 19:00:12 +0400

    Print all the letters except q and e. script 3-print_alphabt.py

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# This script loops through alphabet (ASCII numbers 98-122)
+# except q and e
+# and prints their character values.
+
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue #  If yes, skip it and go to the next number
+    #  "{:c}" is a format code that means "convert this
+    print("{:c}".format(i), end="")
