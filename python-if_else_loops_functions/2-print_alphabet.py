3867bda26085782e3525415702c84aa1024e0ff8
telmanhaji <ibliandr@gmail.com>
2025-11-15 18:31:11 +0400

    script 2-print_alphabet.py that print all alphabet without adding new line. Pycodestyle E231 should be passed

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -6,4 +6,4 @@ for i in range(97, 123):
     # chr(i) turns the number (e.g., 97) into a letter ('a')
     # end="" prints it without a new line, so they all
     # appear on the same line.
-    print(chr(i), end="") # Correct
+    print(chr(i), end="") #  Correct!
