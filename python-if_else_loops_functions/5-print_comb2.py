76409eab30238d2adf21544bc87a4d829d0a260a
telmanhaji <ibliandr@gmail.com>
2025-11-15 20:09:22 +0400

    This script prints numbers 0-99 with 2 digits and commas.

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script prints numbers 0-99 with 2 digits and commas.
+# This script displays numbers 0-99 with 2 digits and commas.
 
 for i in range(100):
     if i == 99:
