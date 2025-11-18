8491c2a06186ea30d315d472aea37a5b9f343856
telmanhaji <ibliandr@gmail.com>
2025-11-18 23:09:52 +0400

    commited 1-element_at.py for task 1. Secure access to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """ Return element at given idx """
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    return my_list[idx]
