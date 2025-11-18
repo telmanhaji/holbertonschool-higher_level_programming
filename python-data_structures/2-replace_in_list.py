4bf9e41b796a97b83d2a45a77087bd92fc14534f
telmanhaji <ibliandr@gmail.com>
2025-11-18 23:26:20 +0400

    commited 2-replace_in_list.py for 2. Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
     """
     Replaces an element of a list at a specific position
     """
-    if idx <0:
+    if idx < 0:
         return my_list
     if idx >= len(my_list):
         return my_list
