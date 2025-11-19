45aab8b41a09cc4396b9c8645c76bffcf62dd8c8
telmanhaji <ibliandr@gmail.com>
2025-11-19 10:18:22 +0400

    commited 4-new_in_list.py for "Task 4. Replace in a copy"

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""
-Replaces an element in a list at a specific position
-without modifying the original list
-"""
-# 1st. Creating a copy of the original list immediately.
-# Using the slicing syntax [:] to clone the list
-new_list = my_list [:]
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element in a list at a specific position
+    without modifying the original list.
+    """
+    # 1st. Creates a copy of the original list immediately.
+    # Should use the slicing syntax [:] to clone the list.
+    new_list = my_list[:]
 
-# 2nd. Checking if the index is valid
-if idx < 0:
-    return new_list
-if idx >= len (my_list):
-    return new_list
-# 3rd. If valid, making the change ONLY to the new_list
-new_list[idx] = element
+    # 2nd. Checks if the index is valid
+    if idx < 0:
+        return new_list
+    if idx >= len(my_list):
+        return new_list
 
-# 4th. Returning the modified copy
-return new_list
+    # 3rd. If valid, makes the change ONLY to the new_list
+    new_list[idx] = element
+
+    # 4th. Return the modified copy
+    return new_list
