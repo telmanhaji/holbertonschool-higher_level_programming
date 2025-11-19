687dfc08b3091d043124bf897871d2fc99fe106e
telmanhaji <ibliandr@gmail.com>
2025-11-19 09:40:20 +0400

    commited 3-print_reversed_list_integer.py for "Task 3. Print a list of integers... in reverse!"

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """
+    Outputs all integers of a list in reverse order
+    """
+    for i in reversed(my_list):
+        print("{:d}".format(i))
