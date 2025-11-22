fb9adfa1064dee4edec75a1091aa0c0f2855c13e
telmanhaji <ibliandr@gmail.com>
2025-11-22 14:15:50 +0400

    commited a module 5-raise_exception.py, that raises a TypeError exception. It`s a solution for "task 5. Raise exception"

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    This module raises a TypeError exception.
+    """
+    # the 'raise' keyword allows us to manually trigger an exception.
+    # it is forcing the program to report a TypeError right here, right now.
+    raise TypeError
