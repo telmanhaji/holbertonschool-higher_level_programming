5848ffb927c5c47c268addf82b0b839b60faf5e9
telmanhaji <ibliandr@gmail.com>
2025-11-22 15:48:08 +0400

    commited a module 102-magic_calculation.py, that does exactly the same as the following Python bytecode in example of task "8. Safe function"

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                # note: the bytecode for the math starts at offset 58.
+                # the IF check jumps to 58 if False.
+                # so the math happens if the exception is NOT raised.
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
