60eba9d964a95e68a56fb2719bd8cde64a1bf18f
telmanhaji <ibliandr@gmail.com>
2025-11-24 16:35:18 +0400

    0-rectangle.py defines an empty class Rectangle as a solution to task 0. Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this module defines an empty class Rectangle.
+it serves as the blueprint for future geometric operations.
+"""
+
+
+class Rectangle:
+    """
+    empty class Rectangle that defines a rectangle.
+    """
+    pass
