47f3213c9d42a10da9497f1a2cf7c62305a1f810
telmanhaji <ibliandr@gmail.com>
2025-11-24 23:09:32 +0400

    9-square.py defines a class Square that inherits from Rectangle as a solution to task 9. A square is a rectangle

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this module defines a class Square that inherits from Rectangle.
+it demonstrates the power of inheritance to reuse code.
+"""
+# it must import the parent class to use it
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    represents a square.
+    inherits all attributes and methods (validation, area, etc.) from Rectangle.
+    """
+
+    def __init__(self, size=0):
+        """
+        initializes the square.
+
+        args:
+            size (int): the size of the square. defaults to 0.
+        """
+        # attention!
+        # it calls the Parent's (super) constructor.
+        # rectangle expects (width, height).
+        # it passes (size, size).
+        # this automatically triggers all the validation logic in Rectangle!
+        super().__init__(size, size)
