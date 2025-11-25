ce6df8f10455e2c042f7126fa77a05b27cf0f4d0
telmanhaji <ibliandr@gmail.com>
2025-11-25 19:27:39 +0400

    2-s_same_class.py module returns True if the object is exactly an instance of the specified class otherwise False. It`s solution for task 2. Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """
+    this module returns True if the object is exactly an instance of the specified class;
+    otherwise False.
+    """
+    # it compares the type of the object directly to the class.
+    # it does NOT use isinstance() because that allows inheritance.
+    return type(obj) == a_class
