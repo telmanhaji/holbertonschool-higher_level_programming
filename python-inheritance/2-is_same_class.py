934f092f8afdf83613f1de916d871fe5c0c8d5a3
telmanhaji <ibliandr@gmail.com>
2025-11-25 19:34:52 +0400

    2-s_same_class.py module defines a function for strict type checking.. It`s solution for task 2. Exact same object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+this module defines a function for strict type checking.
+it is used to verify if an object is exactly an instance of a class.
+"""
+
+
 def is_same_class(obj, a_class):
     """
     returns True if the object is exactly an instance of the specified class;
     otherwise False.
+
+    args:
+        obj (any): the object to check.
+        a_class (type): The class to match against.
+
+    returns:
+        bool: True if obj is exactly an instance of a_class, False otherwise.
     """
-    # it compares the type of the object directly to the class.
-    # it does NOT use isinstance() because that allows inheritance.
     return type(obj) == a_class
