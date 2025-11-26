1c91d457e494ced2b9030c51835f31f725f8acbb
telmanhaji <ibliandr@gmail.com>
2025-11-26 23:36:33 +0400

    commited 1 module for task 13. Can I?

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines a function that attempts to add a new attribute to an object.
+It demonstrates checking object capabilities/mutability before assignment.
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Adds a new attribute to an object if it's possible.
+
+    Args:
+        obj (any): The object to add the attribute to.
+        name (str): The name of the attribute.
+        value (any): The value of the attribute.
+
+    Raises:
+        TypeError: If the object cannot have new attributes.
+    """
+    # In Python, objects that allow arbitrary new attributes usually
+    # have a __dict__ attribute. Built-in types (str, int) do not.
+    # Slots are a special case, but checking __dict__ covers the requirement here.
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+
+    # If we passed the check, we use setattr to perform the injection
+    setattr(obj, name, value)
