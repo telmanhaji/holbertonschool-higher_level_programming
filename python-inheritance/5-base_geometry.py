284d06bb1663fb875c94e49224e81dd811d7c0b4
telmanhaji <ibliandr@gmail.com>
2025-11-26 22:04:29 +0400

    commited 2 files: 5-base_geometry.txt and 5-base_geometry.py for task 5. Geometry module

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+It serves as the foundation for a geometry framework.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that defines the base geometry.
+    """
+    pass
