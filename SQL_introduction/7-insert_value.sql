0fddda8cda1d860cc3defc62a7cc6e49af8f6bce
telmanhaji <ibliandr@gmail.com>
2025-11-28 15:59:00 +0400

    commited SQL script 7-insert_value.sql that inserts a new row in the table first_table in server. It`s a solution for Task 7. First add" in project "SQL - Introduction"

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table first_table in server.
-INSERT INTO first_table (id, name) and VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
