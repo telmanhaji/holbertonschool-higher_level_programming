b4767ec4e6b765eda66d18dba5e6b9d9c99d7f3e
telmanhaji <ibliandr@gmail.com>
2025-11-28 17:01:13 +0400

    commited SQL script 10-top_score.sql that Lists all records of the table second_table of the DB and sort by score, results display both the score and the name. Records ordered by score. It`s a solution for Task "10. List by best" in project "SQL - Introduction"

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table second_table of the DB and sort by score.
+-- Results should display both the score and the name.
+-- Records should be ordered by score (top first).
+SELECT score, name FROM second_table ORDER BY score DESC;
