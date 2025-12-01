4a938d5147c20e3a30a3ee9fd920306aaefcff5f
Telman H. <ibliandr@gmail.com>
2025-12-01 22:43:56 +0400

    commited 2-post_email.py module this module sends a POST request to a URL with an email parameter and displays the decoded response body.. It`s a solution for task "2. POST an email #0" project "Python - Network #1"

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
-This script takes a URL and an email, sends a POST request to the passed URL
+this module takes a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of the response.
 """
 import urllib.request
@@ -8,23 +8,23 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    # 1. Get arguments
+    # get arguments
     url = sys.argv[1]
     email = sys.argv[2]
 
-    # 2. Prepare the Data
+    # prepare the Data
     # We create a dictionary of our data
     values = {'email': email}
 
-    # We must encode the data into a query string (format: key=value)
+    # it  encodes the data into a query string (format: key=value)
     # and then encode that string into bytes (ascii)
     data = urllib.parse.urlencode(values).encode('ascii')
 
-    # 3. Create the Request object
+    # creates the Request object
     # By passing the 'data' argument, urllib automatically uses the POST method
     req = urllib.request.Request(url, data)
 
-    # 4. Send the Request and Read Response
+    # send the Request and Read Response
     with urllib.request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
